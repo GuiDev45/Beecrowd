7001fdd79bb8587e64f2700e3cf71ee212f2b43e
GuiDev45 <bortolettodev@gmail.com>
2022-04-25 11:49:16 -0300

    Problemas resolvidos

---
+++ Beecrowd-Nivel-Iniciante/1013/1013/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _1013
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
+            MaiorAB = (a+b+abs(a-b))/2
+
+            Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). 
+            Um segundo passo, portanto é necessário para chegar no resultado esperado.
+
+            Entrada
+            O arquivo de entrada contém três valores inteiros.
+
+            Saída
+            Imprima o maior dos três valores seguido por um espaço e a mensagem "eh o maior".
+            
+            Exemplos de Entrada             Exemplos de Saída
+            7 14 106                        106 eh o maior                       
+            -------------------             -------------------
+            217 14 6                        217 eh o maior                       
+            */
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            int A = int.Parse(vet[0]);
+            int B = int.Parse(vet[1]);
+            int C = int.Parse(vet[2]);
+
+            int maiorAB = (A + B + Math.Abs(A - B)) / 2;
+            int maiorAC = (A + C + Math.Abs(A - C)) / 2;
+
+            int maior = Math.Max(maiorAB, maiorAC);
+
+            Console.WriteLine(maior + " eh o maior");
+        }
+    }
+}
---
+++ Beecrowd-Nivel-Iniciante/1015/1015/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Leia os quatro valores correspondentes aos eixos x e y de dois pontos quaisquer no plano, 
+            p1(x1,y1) e p2(x2,y2) e calcule a distância entre eles, mostrando 4 casas decimais após a vírgula, segundo a fórmula:
+
+            Distancia = Raiz quadrada (x2-x1)² + (y2-y1)²
+
+            Entrada
+            O arquivo de entrada contém duas linhas de dados. A primeira linha contém dois valores de ponto flutuante: 
+            x1 y1 e a segunda linha contém dois valores de ponto flutuante x2 y2.
+
+            Saída
+            Calcule e imprima o valor da distância segundo a fórmula fornecida, com 4 casas após o ponto decimal.
+
+            Exemplos de Entrada             Exemplos de Saída
+            1.0 7.0                         4.4721
+            5.0 9.0                       
+            -------------------             -------------------
+            -2.5 0.4                        16.1484 
+            12.1 7.3
+            -------------------             -------------------
+            2.5 -0.4                        16.4575
+            -12.2 7.0
+            */
+
+            double x1, x2, y1, y2, distancia;
+
+            string[] vet;
+
+            vet = Console.ReadLine().Split(' ');
+            x1 = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            y1 = double.Parse(vet[1], CultureInfo.InvariantCulture);
+
+            vet = Console.ReadLine().Split(' ');
+            x2 = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            y2 = double.Parse(vet[1], CultureInfo.InvariantCulture);
+
+            distancia = Math.Sqrt(Math.Pow(x1 - x2, 2.00) + Math.Pow(y1 - y2, 2.00));
+
+            Console.WriteLine(distancia.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}

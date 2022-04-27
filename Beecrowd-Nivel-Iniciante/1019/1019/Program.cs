c1493e3a98d3cfe92a8dba946b6448645d2112b8
GuiDev45 <bortolettodev@gmail.com>
2022-04-27 11:30:22 -0300

    Problemas resolvidos

---
+++ Beecrowd-Nivel-Iniciante/1019/1019/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _1019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica, e informe-o expresso no formato horas:minutos:segundos.
+
+            Entrada
+            O arquivo de entrada contém um valor inteiro N.
+
+            Saída
+            Imprima o tempo lido no arquivo de entrada (segundos), convertido para horas:minutos:segundos, conforme exemplo fornecido.
+            
+            Exemplos de Entrada             Exemplos de Saída
+            556                             0:9:16                   
+            -------------------             -------------------
+            1                               0:0:1
+            -------------------             -------------------
+            140153                          38:55:53
+            */
+
+            int N = int.Parse(Console.ReadLine());
+
+            int horas = N / 3600;
+            int resto = N % 3600;
+
+            int minutos = resto / 60;
+            int segundos = resto % 60;
+
+            Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+        }
+    }
+}
---
+++ Beecrowd-Nivel-Iniciante/1020/1020/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _1020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a em anos, meses e dias
+
+            Obs.: apenas para facilitar o cálculo, considere todo ano com 365 dias e todo mês com 30 dias. 
+            Nos casos de teste nunca haverá uma situação que permite 12 meses e alguns dias, como 360, 363 ou 364. 
+            Este é apenas um exercício com objetivo de testar raciocínio matemático simples.
+
+            Entrada
+            O arquivo de entrada contém um valor inteiro.
+
+            Saída
+            Imprima a saída conforme exemplo fornecido.
+
+            Exemplos de Entrada             Exemplos de Saída
+            400                             1 ano(s)
+                                            1 mes(es)
+                                            5 dia(s)                       
+            -------------------             -------------------
+            800                             2 ano(s)
+                                            2 mes(es)
+                                            10 dia(s) 
+            -------------------             -------------------
+            30                              0 ano(s)
+                                            1 mes(es)
+                                            0 dia(s)
+            */
+
+            int ano = 365;
+            int mes = 30;
+
+            int idade = int.Parse(Console.ReadLine());
+
+            int quociente = idade / ano;
+            Console.WriteLine(quociente + " ano(s)");
+            int resto = idade % ano;
+
+            quociente = resto / mes;
+            Console.WriteLine(quociente + " mes(es)");
+            resto = resto % mes;
+
+            Console.WriteLine(resto + " dia(s)");
+        }
+    }
+}

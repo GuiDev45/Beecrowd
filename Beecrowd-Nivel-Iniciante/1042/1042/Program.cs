b184846bb6a6a6e3f9adf4fae5da03012f2fce2e
GuiDev45 <bortolettodev@gmail.com>
2022-05-01 22:05:00 -0300

    Problemas resolvidos

---
+++ Beecrowd-Nivel-Iniciante/1042/1042/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace _1042
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Leia 3 valores inteiros e ordene-os em ordem crescente. No final, mostre os valores em ordem crescente, uma linha em branco e em seguida, os valores na sequência como foram lidos.
+
+            Entrada
+            A entrada contem três números inteiros.
+
+            Saída
+            Imprima a saída conforme foi especificado.
+            
+            Exemplos de Entrada             Exemplos de Saída
+            7 21 -14                        -14
+                                            7
+                                            21
+
+                                            7
+                                            21
+                                            -14                  
+            -------------------             -------------------
+            -14 21 7                        -14
+                                            7
+                                            21
+
+                                            -14
+                                            21
+                                            7
+            */
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            int num1 = int.Parse(vet[0]);
+            int num2 = int.Parse(vet[1]);
+            int num3 = int.Parse(vet[2]);
+
+            int menor = 0, medio = 0, maior = 0;
+
+            if (num1 < num2 && num1 < num3 && num2 < num3)
+            {
+                menor = num1;
+                medio = num2;
+                maior = num3;
+            }
+            else if (num1 < num2 && num1 < num3 && num2 > num3)
+            {
+                menor = num1;
+                medio = num3;
+                maior = num2;
+            }
+            else if (num1 > num2 && num1 < num3 && num2 < num3)
+            {
+                menor = num2;
+                medio = num1;
+                maior = num3;
+            }
+            else if (num1 > num2 && num1 > num3 && num2 < num3)
+            {
+                menor = num2;
+                medio = num3;
+                maior = num1;
+            }
+            else if (num1 > num2 && num1 > num3 && num2 > num3)
+            {
+                menor = num3;
+                medio = num2;
+                maior = num1;
+            }
+            else
+            {
+                menor = num3;
+                medio = num1;
+                maior = num2;
+            }
+
+            Console.WriteLine(menor);
+            Console.WriteLine(medio);
+            Console.WriteLine(maior);
+
+            Console.WriteLine();
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+        }
+    }
+}
---
+++ Beecrowd-Nivel-Iniciante/1043/1043/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1043
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Leia 3 valores reais (A, B e C) e verifique se eles formam ou não um triângulo. Em caso positivo, calcule o perímetro do triângulo e apresente a mensagem:
+            Perimetro = XX.X
+            Em caso negativo, calcule a área do trapézio que tem A e B como base e C como altura, mostrando a mensagem
+            Area = XX.X
+
+            Entrada
+            A entrada contém três valores reais.
+
+            Saída
+            O resultado deve ser apresentado com uma casa decimal.
+            
+            Exemplos de Entrada             Exemplos de Saída
+            6.0 4.0 2.0                     Area = 10.0                  
+            -------------------             -------------------
+            6.0 4.0 2.1                     Perimetro = 12.1
+            */
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            double A = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            double B = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            double C = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            if (A < B + C && A > Math.Abs(B - C))
+            {
+                double perimetro = A + B + C;
+                Console.WriteLine("Perimetro = " + perimetro.ToString("F1", CultureInfo.InvariantCulture));
+
+            }
+            else
+            {
+                double area = (A + B) / 2.0 * C;
+                Console.WriteLine("Area = " + area.ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}

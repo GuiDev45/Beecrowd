4fa0977c0769efb77ea61d9f7d20e857475e9676
GuiDev45 <bortolettodev@gmail.com>
2022-05-03 01:55:38 -0300

    Problemas resolvidos

---
+++ Beecrowd-Nivel-Iniciante/1048/1048/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1048
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*A empresa ABC resolveu conceder um aumento de salários a seus funcionários de acordo com a tabela abaixo:
+
+            Salário	                            Percentual de Reajuste
+            0 - 400.00                                  15%
+            400.01 - 800.00                             12%
+            800.01 - 1200.00                            10%
+            1200.01 - 2000.00                           7%
+            Acima de 2000.00                            4%
+
+            Leia o salário do funcionário e calcule e mostre o novo salário, 
+            bem como o valor de reajuste ganho e o índice reajustado, em percentual.
+
+            Entrada
+            A entrada contém apenas um valor de ponto flutuante, com duas casas decimais.
+
+            Saída
+            Imprima 3 linhas na saída: o novo salário, o valor ganho de reajuste e o percentual de reajuste ganho, conforme exemplo abaixo.
+            
+            Exemplos de Entrada             Exemplos de Saída
+            400.00                          Novo salario: 460.00
+                                            Reajuste ganho: 60.00
+                                            Em percentual: 15 %                
+            -------------------             -------------------
+            800.01                          Novo salario: 880.01
+                                            Reajuste ganho: 80.00
+                                            Em percentual: 10 %
+            -------------------             -------------------
+            2000.00                     	Novo salario: 2140.00
+                                            Reajuste ganho: 140.00
+                                            Em percentual: 7 %
+            */
+
+            int porcentagem;
+            double reajuste;
+
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (salario > 0.0 && salario <= 400.0)
+            {
+                porcentagem = 15;
+                reajuste = (salario * porcentagem) / 100.0;
+
+                Console.WriteLine("Novo salario: " + (salario + reajuste).ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + reajuste.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual: " + porcentagem + " %");
+            }
+            else if (salario > 400.0 && salario <= 800.0)
+            {
+                porcentagem = 12;
+                reajuste = (salario * porcentagem) / 100.0;
+
+                Console.WriteLine("Novo salario: " + (salario + reajuste).ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + reajuste.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual: " + porcentagem + " %");
+            }
+            else if (salario > 800.0 && salario <= 1200.0)
+            {
+                porcentagem = 10;
+                reajuste = (salario * porcentagem) / 100.0;
+
+                Console.WriteLine("Novo salario: " + (salario + reajuste).ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + reajuste.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual: " + porcentagem + " %");
+            }
+            else if (salario > 1200.0 && salario <= 2000.0)
+            {
+                porcentagem = 7;
+                reajuste = (salario * porcentagem) / 100.0;
+
+                Console.WriteLine("Novo salario: " + (salario + reajuste).ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + reajuste.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual: " + porcentagem + " %");
+            }
+            else
+            {
+                porcentagem = 4;
+                reajuste = (salario * porcentagem) / 100.0;
+
+                Console.WriteLine("Novo salario: " + (salario + reajuste).ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + reajuste.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual: " + porcentagem + " %");
+            }
+        }
+    }
+}
---
+++ Beecrowd-Nivel-Iniciante/1049/1049/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace _1049
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Neste problema, você deverá ler 3 palavras que definem o tipo de animal possível segundo o esquema abaixo, da esquerda para a direita.  Em seguida conclua qual dos animais seguintes foi escolhido, através das três palavras fornecidas.
+
+            Vertebrado - Ave - Carnívoro = Águia
+            Vertebrado - Ave - Onivoro = Pomba
+            Vertebrado - Mamífero - Onivoro = Homem
+            Vertebrado - Mamífero - Herbívoro = Vaca
+            Invertebrado - Inseto - Hematofago = Pulga
+            Invertebrado - Inseto - Herbívoro = Lagarta
+            Invertebrado - Anelídeo - Hematofago = Sanguessuga
+            Invertebrado - Anelídeo - Onivoro = Minhoca
+
+            Entrada
+            A entrada contém 3 palavras, uma em cada linha, necessárias para identificar o animal segundo a figura acima, com todas as letras minúsculas.
+
+            Saída
+            Imprima o nome do animal correspondente à entrada fornecida.
+            
+            Exemplos de Entrada             Exemplos de Saída
+            vertebrado                      homem
+            mamifero
+            onivoro                         
+               
+            -------------------             -------------------
+            vertebrado                      aguia
+            ave
+            carnivoro
+            -------------------             -------------------
+            invertebrado                    minhoca
+            anelideo
+            onivoro
+            */
+
+            string palavra1 = Console.ReadLine();
+            string palavra2 = Console.ReadLine();
+            string palavra3 = Console.ReadLine();
+
+            if (palavra1 == "vertebrado" && palavra2 == "ave" && palavra3 == "carnivoro")
+            {
+                Console.WriteLine("aguia");
+            }
+            else if (palavra1 == "vertebrado" && palavra2 == "ave" && palavra3 == "onivoro")
+            {
+                Console.WriteLine("pomba");
+            }
+
+            if (palavra1 == "vertebrado" && palavra2 == "mamifero" && palavra3 == "onivoro")
+            {
+                Console.WriteLine("homem");
+            }
+            else if (palavra1 == "vertebrado" && palavra2 == "mamifero" && palavra3 == "herbivoro")
+            {
+                Console.WriteLine("vaca");
+            }
+
+            if (palavra1 == "invertebrado" && palavra2 == "inseto" && palavra3 == "hematofago")
+            {
+                Console.WriteLine("pulga");
+            }
+            else if (palavra1 == "invertebrado" && palavra2 == "inseto" && palavra3 == "herbivoro")
+            {
+                Console.WriteLine("lagarta");
+            }
+
+            if (palavra1 == "invertebrado" && palavra2 == "anelideo" && palavra3 == "hematofago")
+            {
+                Console.WriteLine("sanguessuga");
+            }
+            else if (palavra1 == "invertebrado" && palavra2 == "anelideo" && palavra3 == "onivoro")
+            {
+                Console.WriteLine("minhoca");
+            }
+        }
+    }
+}

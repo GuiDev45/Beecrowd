89eb3e07aa345687329132de9144433f54042318
GuiDev45 <bortolettodev@gmail.com>
2022-05-06 07:44:30 -0300

    Problemas resolvidos

---
+++ Beecrowd-Nivel-Iniciante/1065/1065/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _1065
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Faça um programa que leia 5 valores inteiros. Conte quantos destes valores digitados são pares e mostre esta informação.
+
+            Entrada
+            O arquivo de entrada contém 5 valores inteiros quaisquer.
+
+            Saída
+            Imprima a mensagem conforme o exemplo fornecido, indicando a quantidade de valores pares lidos.
+            
+            Exemplos de Entrada             Exemplos de Saída
+            7                               3 valores pares
+            -5
+            6
+            -4
+            12
+            */
+
+            int valorInteiro = 0;
+
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+            int num5 = int.Parse(Console.ReadLine());
+
+            if (num1 % 2 == 0)
+            {
+                valorInteiro = valorInteiro + 1;
+            }
+
+            if (num2 % 2 == 0)
+            {
+                valorInteiro = valorInteiro + 1;
+            }
+
+            if (num3 % 2 == 0)
+            {
+                valorInteiro = valorInteiro + 1;
+            }
+
+            if (num4 % 2 == 0)
+            {
+                valorInteiro = valorInteiro + 1;
+            }
+
+            if (num5 % 2 == 0)
+            {
+                valorInteiro = valorInteiro + 1;
+            }
+
+            Console.WriteLine(valorInteiro + " valores pares");
+        }
+    }
+}
---
+++ Beecrowd-Nivel-Iniciante/1066/1066/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _1066
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Leia 5 valores Inteiros. A seguir mostre quantos valores digitados foram pares, 
+            quantos valores digitados foram ímpares, quantos valores digitados foram positivos e quantos valores digitados foram negativos.
+
+            Entrada
+            O arquivo de entrada contém 5 valores inteiros quaisquer.
+
+            Saída
+            Imprima a mensagem conforme o exemplo fornecido, uma mensagem por linha, não esquecendo o final de linha após cada uma.
+            
+            Exemplos de Entrada             Exemplos de Saída
+            -5                              3 valor(es) par(es)
+            0                               2 valor(es) impar(es)
+            -3                              1 valor(es) positivo(s)
+            -4                              3 valor(es) negativo(s)
+            12
+            */
+
+            int valorPar = 0;
+            int valorImpar = 0;
+            int valorPositivo = 0;
+            int valorNegativo = 0;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num % 2 == 0)
+                {
+                    valorPar++;
+                }
+                else
+                {
+                    valorImpar++;
+                }
+
+                if (num > 0)
+                {
+                    valorPositivo++;
+                }
+                else if (num < 0)
+                {
+                    valorNegativo++;
+                }
+            }
+
+            Console.WriteLine(valorPar + " valor(es) par(es)");
+            Console.WriteLine(valorImpar + " valor(es) impar(es)");
+            Console.WriteLine(valorPositivo + " valor(es) positivo(s)");
+            Console.WriteLine(valorNegativo + " valor(es) negativo(s)");
+
+        }
+    }
+}

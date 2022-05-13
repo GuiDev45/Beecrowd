971ce30f45b499988e0e84a5dda239646284c5c3
GuiDev45 <bortolettodev@gmail.com>
2022-05-13 05:58:22 -0300

    Problemas resolvidos

---
+++ Beecrowd-Nivel-Iniciante/1101/1101/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _1101
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Leia um conjunto não determinado de pares de valores M e N (parar quando algum dos valores for menor ou igual a zero). 
+            Para cada par lido, mostre a sequência do menor até o maior e a soma dos inteiros consecutivos entre eles (incluindo o N e M).
+
+            Entrada
+            O arquivo de entrada contém um número não determinado de valores M e N. A última linha de entrada vai conter um número nulo ou negativo.
+
+            Saída
+            Para cada dupla de valores, imprima a sequência do menor até o maior e a soma deles, conforme exemplo abaixo.
+            
+            Exemplos de Entrada             Exemplos de Saída
+            5 2                             2 3 4 5 Sum=14
+            6 3                             3 4 5 6 Sum=18
+            5 0
+            */
+
+            int maior, menor, soma;
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            int M = int.Parse(vet[0]);
+            int N = int.Parse(vet[1]);
+
+            while (M > 0 && N > 0)
+            {
+                soma = 0;
+                maior = 0;
+                menor = 0;
+
+                if (M >= N)
+                {
+                    maior = M;
+                    menor = N;
+                }
+                else
+                {
+                    maior = N;
+                    menor = M;
+                }
+
+                for (int i = menor; i <= maior; i++)
+                {
+                    Console.Write(i + " ");
+                    soma += i;
+                }
+
+                Console.WriteLine("Sum=" + soma);
+
+                vet = Console.ReadLine().Split(' ');
+
+                M = int.Parse(vet[0]);
+                N = int.Parse(vet[1]);
+            }
+        }
+    }
+}
---
+++ Beecrowd-Nivel-Iniciante/1113/1113/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _1113
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Leia uma quantidade indeterminada de duplas de valores inteiros X e Y. 
+            Escreva para cada X e Y uma mensagem que indique se estes valores foram digitados em ordem crescente ou decrescente.
+
+            Entrada
+            A entrada contém vários casos de teste. 
+            Cada caso contém dois valores inteiros X e Y. 
+            A leitura deve ser encerrada ao ser fornecido valores iguais para X e Y.
+
+            Saída
+            Para cada caso de teste imprima “Crescente”, 
+            caso os valores tenham sido digitados na ordem crescente, 
+            caso contrário imprima a mensagem “Decrescente”.
+            
+            Exemplos de Entrada             Exemplos de Saída
+            5 4                             Decrescente
+            7 2                             Decrescente
+            3 8                             Crescente
+            2 2
+            */
+
+            string[] valores = Console.ReadLine().Split(' ');
+
+            int X = int.Parse(valores[0]);
+            int Y = int.Parse(valores[1]);
+
+            while (X != Y)
+            {
+                if (X < Y)
+                {
+                    Console.WriteLine("Crescente");
+                }
+                else
+                {
+                    Console.WriteLine("Decrescente");
+                }
+
+                valores = Console.ReadLine().Split(' ');
+
+                X = int.Parse(valores[0]);
+                Y = int.Parse(valores[1]);
+            }
+        }
+    }
+}

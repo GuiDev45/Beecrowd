92561107fa60be2e6ef32e28d77d2673ccd01b38
GuiDev45 <bortolettodev@gmail.com>
2022-06-03 09:09:35 -0300

    Problemas resolvidos

---
+++ Beecrowd-Nivel-Iniciante-C-Sharp/1143/1143/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _1143
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escreva um programa que leia um valor inteiro N (1 < N < 1000). Este N é a quantidade de linhas de saída que serão apresentadas na execução do programa.
+
+            Entrada
+            O arquivo de entrada contém um número inteiro positivo N.
+
+            Saída
+            Imprima a saída conforme o exemplo fornecido.
+            
+            Exemplos de Entrada             Exemplos de Saída
+            5                               1 1 1
+                                            2 4 8
+                                            3 9 27
+                                            4 16 64
+                                            5 25 125
+            */
+
+            int m1, m2;
+
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                m1 = (int)Math.Pow(i, 2);
+                m2 = m1 * i;
+                Console.Write(i + " ");
+                Console.Write(m1 + " ");
+                Console.WriteLine(m2);
+            }
+        }
+    }
+}
---
+++ Beecrowd-Nivel-Iniciante-C-Sharp/1149/1149/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _1149
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Faça um algoritmo para ler um valor A e um valor N. Imprimir a soma de A + i para cada i com os valores (0 <= i <= N-1). 
+            Enquanto N for negativo ou ZERO, um novo N(apenas N) deve ser lido.
+
+            Entrada
+            A entrada contém somente valores inteiros, podendo ser positivos ou negativos. Todos os valores estão na mesma linha.
+
+            Saída
+            A saída contém apenas um valor inteiro.
+            
+            Exemplos de Entrada             Exemplos de Saída
+            3 2                             7
+            -------------------             -------------------
+            3 -1 0 -2 2                     7
+            */
+
+            string[] vet = Console.ReadLine().Split(' ');
+            int a = int.Parse(vet[0]);
+            int n = int.Parse(vet[1]);
+
+            int cont = 1;
+
+            while (n <= 0)
+            {
+                cont++;
+                n = int.Parse(vet[cont]);
+
+            }
+
+            int soma = 0;
+
+            for (int i = 0; i <= n - 1; i++)
+            {
+                soma = soma + (a + i);
+
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
